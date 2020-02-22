f8a9f6ba5e0421e32aae7a891812f3d9b92b75f9
Psage23 <pamelasage3@gmail.com>
2020-02-22 13:07:32 -0700

    login and register work

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const jwt = require('jsonwebtoken');
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
@@ -14,5 +14,23 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get('/', (req, res) => {
+    res.send("Server is talking");
+})
+
+server.get("/token", (req,res) => {
+    const payload = {
+        subject: 'thisuser',
+        userid: 'psage',
+        favoriteChili: 'green'
+    }
+    const secret = 'sprintChallengeSecret';
+    const options = {
+        expiresIn: '8h'
+    };
+    const token = jwt.sign(payload, secret, options);
+    res.json(token);
+})
 
 module.exports = server;
